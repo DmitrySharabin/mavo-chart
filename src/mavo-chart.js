3af9683228f48b9967e8ccdea66aebd27fbfe5fe
Dmitry Sharabin <dmitrysharabin@gmail.com>
2019-07-22 23:16:14 +0300

    Alpha version with observers

---
+++ src/mavo-chart.js
@@ -0,0 +1,110 @@
+(function ($) {
+    const SELECTOR = 'canvas.mv-chart, canvas[mv-chart-data]';
+
+    Mavo.Plugins.register('chart', {
+        dependencies: [
+            'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js',
+            'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.css'
+        ]
+    });
+
+    Mavo.Elements.register('chart', {
+        default: true,
+        selector: SELECTOR,
+        attribute: null,
+        hasChildren: true,
+        modes: 'read', // Charts are read-only!
+
+        init: function () {
+
+            // Default chart
+            const chartObj = {
+                type: 'line',
+                options: {
+                    title: {
+                        position: this.element.getAttribute('mv-chart-title-position') || 'top'
+                    },
+                    legend: {
+                        position: this.element.getAttribute('mv-chart-legend-position') || 'top'
+                    }
+                }
+            }
+
+            const datasets = [];
+
+            // Parse styles of series of data
+            const seriesStyles = this.element.getAttribute('mv-chart-series-styles');
+            if (seriesStyles) {
+                seriesStyles
+                    .replace(/\s{2,}/g, ' ')
+                    .trim()
+                    .split(';').forEach(style => {
+                        // Known issue: can't use color functions (e.g., rgb())
+                        datasets.push(Mavo.options(style));
+                    });
+            }
+
+            this.chart = new Chart(this.element.getContext('2d'), chartObj);
+            $.extend(this.chart.data.datasets, datasets);
+
+            // Observers for live attributes
+            if (this.element.hasAttribute('mv-chart-type')) {
+                this.chartTypeObserver = new Mavo.Observer(this.element, 'mv-chart-type', () => {
+                    this.chart.config.type = this.element.getAttribute('mv-chart-type');
+                    this.chart.update();
+                });
+            }
+
+            if (this.element.hasAttribute('mv-chart-data')) {
+                this.chartDataObserver = new Mavo.Observer(this.element, 'mv-chart-data', () => {
+                    this.element.getAttribute('mv-chart-data')
+                    .split(';')
+                    .forEach((dataset, index) => {
+                        const data = dataset.split(',').map(num => +num);
+                        this.chart.data.datasets[index] = { ...this.chart.data.datasets[index], data };
+                    });
+                    this.chart.update();
+                });
+            }
+
+            if (this.element.hasAttribute('mv-chart-labels')) {
+                this.chartLabelsObserver = new Mavo.Observer(this.element, 'mv-chart-labels', () => {
+                    this.chart.data.labels = this.element.getAttribute('mv-chart-labels').split(',');
+                    this.chart.update();
+                });
+            }
+
+            if (this.element.hasAttribute('mv-chart-title')) {
+                this.chartTitleObserver = new Mavo.Observer(this.element, 'mv-chart-title', () => {
+                    const title = this.element.getAttribute('mv-chart-title').replace(/\s{2,}/g, ' ').trim();
+                    if (title === '') {
+                        this.chart.options.title.display = false;
+                    } else {
+                        this.chart.options.title.text = title;
+                        this.chart.options.title.display = true;
+                    }
+                    this.chart.update();
+                });
+            }
+
+            if (this.element.hasAttribute('mv-chart-legend')) {
+                this.chartLegendObserver = new Mavo.Observer(this.element, 'mv-chart-legend', () => {
+                    const legend = this.element.getAttribute('mv-chart-legend').replace(/\s{2,}/g, ' ').trim();
+                    if (legend === '') {
+                        this.chart.options.legend.display = false;
+                    } else {
+                        this.chart.options.legend.display = true;
+                        legend
+                            // What if labels contain semicolon inside?
+                            .split(';')
+                            .forEach((label, index) => {
+                                this.chart.data.datasets[index] = { ...this.chart.data.datasets[index], label: label.trim() };
+                            });
+                    }
+                    this.chart.update();
+                });
+            }
+        }
+    });
+
+})(Bliss);
